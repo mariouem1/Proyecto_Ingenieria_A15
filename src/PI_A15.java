de9022b6bd79e729a92da271dcefc90f4d6e2435
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-05-02 00:35:11 +0200

    V1.0.0

---
+++ src/PI_A15.java
@@ -0,0 +1,43 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PI_A15 {
+
+	private static String APIKey1="1587f1c706adc7607d141a21f87bd306";	//Colocación de la API Key de OpenWeather
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+		URL url=new URL("https://api.openweathermap.org/data/2.5/forecast?lat=40.416775&lon=-3.703790&lang=es&exclude=daily&appid="+APIKey1+"&units=metric&lang=sp");
+		//Se especifica la logitud, latitud, la respuesat diaria del tiempo, las unidades métricas, y el lenguaje.
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();	
+		connection.setRequestMethod("GET");
+		connection.connect();
+		
+		int respuestaCode=connection.getResponseCode();	//Comentario de todos los erres que puede haber
+		if (respuestaCode==400) {
+			System.out.println("Error 400. Bad Request. Some mandatory parameters in the request are missing or some of request parameters have incorrect format or values out of allowed range. List of all parameters names that are missing or incorrect will be returned in `parameters`attribute of the `ErrorResponse` object");
+		}
+		if (respuestaCode==401) {
+			System.out.println("Error 401. API token did not providen in the request or in case API token provided in the request does not grant access to this API. You must add API token with granted access to the product to the request before returning it.");
+		}
+		if (respuestaCode==404) {
+			System.out.println("Error 404. Data with requested parameters (lat, lon, date etc) does not exist in service database. You must not retry the same request.");
+		}
+		if (respuestaCode==429) {
+			System.out.println("Error 429. Too Many Requests. Key quota of requests for provided API to this API was exceeded. You may retry request after some time or after extending your key quota.");
+		}
+		else{
+			
+		}
+		
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
