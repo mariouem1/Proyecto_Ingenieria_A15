6b864b54de14e2195e8a9fcc8ed35253bc8c1f82
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-05-26 13:49:57 +0200

    V 2.0.3

--- src/PI_A15.java
+++ src/PI_A15.java
@@ -42,31 +42,23 @@ public class PI_A15 {
     }
 	
 	private static String APIKey1="1587f1c706adc7607d141a21f87bd306";	//Colocación de la API Key de OpenWeather
-	private static double Lat=40.416775;
-	private static double Long=-3.703790;
+	private static double Lat=41.36417;
+	private static double Long=2.15056;
+//	Coordenadas de la Torre de Comunicaciones de Montjuïc
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		try {
-		URL url=new URL("https://api.openweathermap.org/data/2.5/forecast?lat="+Lat+"&lon="+Long+"&lang=es&exclude=daily, aletrs&units=metric&appid="+APIKey1);
+		URL url=new URL("https://api.openweathermap.org/data/2.5/forecast?lat="+Lat+"&lon="+Long+"&lang=es&exclude=hourly,daily,aletrs&units=metric&appid="+APIKey1);
 		//Se especifica la logitud, latitud, la respuesta diaria del tiempo, las unidades métricas, y el lenguaje.
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();	
 		connection.setRequestMethod("GET");
 		connection.connect();
 		
 		int respuestaCode=connection.getResponseCode();	//Comentario de todos los erres que puede haber
-		if (respuestaCode==400) {
-			System.out.println("Error 400. Bad Request. Some mandatory parameters in the request are missing or some of request parameters have incorrect format or values out of allowed range. List of all parameters names that are missing or incorrect will be returned in `parameters`attribute of the `ErrorResponse` object");
-		}
-		if (respuestaCode==401) {
-			System.out.println("Error 401. API token did not providen in the request or in case API token provided in the request does not grant access to this API. You must add API token with granted access to the product to the request before returning it.");
-		}
-		if (respuestaCode==404) {
-			System.out.println("Error 404. Data with requested parameters (lat, lon, date etc) does not exist in service database. You must not retry the same request.");
-		}
-		if (respuestaCode==429) {
-			System.out.println("Error 429. Too Many Requests. Key quota of requests for provided API to this API was exceeded. You may retry request after some time or after extending your key quota.");
+		if (respuestaCode != 200) {
+			System.out.println("Code Error: "+respuestaCode);
 		}
 		else{
 			System.out.println(respuestaCode);
@@ -83,18 +75,20 @@ public class PI_A15 {
             // Procesa la respuesta
             String jsonResponse = infoString.toString();
             String temperature = parseValue(jsonResponse, "\"temp\":", ",");
+            String temperaturemin = parseValue(jsonResponse, "\"temp_min\":", ",");
+            String temperaturemax = parseValue(jsonResponse, "\"temp_max\":", ",");
             String humidity = parseValue(jsonResponse, "\"humidity\":", ",");
             String wind = parseWeatherDescription(jsonResponse);
             String description = WeatherDescription(jsonResponse);  
             String time=parseValue(jsonResponse, "\"dt_txt\":", ",");
 
-            
-            System.out.println("Data & Time: " + time);
-            System.out.println("Temperature: " + temperature + " °C");
-            System.out.println("Weather Description: " + description);	
-            //RACIACION UV
-            System.out.println("Humidity: " + humidity + "%");
-            System.out.println("Wind Speed: " + wind +" km/h");
+            System.out.println("Descripción: " + description);	
+            System.out.println("Día & Hora: " + time);
+            System.out.println("Temperatura: " + temperature + " °C");
+            System.out.println("Temperatura Mínima: " + temperaturemin + " °C");
+            System.out.println("Temperatura Máxima: " + temperaturemax + " °C");
+            System.out.println("Humedad: " + humidity + "%");
+            System.out.println("Viento: " + wind +" km/h");
 
 		}
 
